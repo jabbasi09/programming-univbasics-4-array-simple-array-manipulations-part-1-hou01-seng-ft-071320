ea0990fd5e3c2a2243b63404a884c38ac481753c
Junaid Abbasi <juni.abbasi@icloud.com>
2020-06-15 07:34:35 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -3,3 +3,9 @@ def using_push(array, string)
   string = "violet"
   array.push(string)
 end
+
+def using_unshift(array, string)
+  array = ["Brooklyn", "Queens", "Manhattan", "Bronx"]
+  string = "Staten Island"
+  array.unshift(string)
+end
